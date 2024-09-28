c874f71be2d9a1912636735fb20e3348722d462f
Mustahid Hasan <mustahidhasan9@gmail.com>
2024-09-28 17:00:14 +0600

    logout wokrs

--- nms/DASHBOARD/urls.py
+++ nms/DASHBOARD/urls.py
@@ -1,8 +1,10 @@
 # dashboard/urls.py
 
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, logout_view
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),  # Assuming this is the root URL for the dashboard
+    path('logout/', logout_view, name='logout')
+    
 ]
--- nms/DASHBOARD/views.py
+++ nms/DASHBOARD/views.py
@@ -2,7 +2,14 @@
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 @login_required
 def dashboard_view(request):
     return render(request, 'dashboard.html')  # Render the dashboard template
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Replace 'login' with the name of your login URL
