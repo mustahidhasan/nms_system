8e78e4e000ca6dc874de5030efc261facff88c23
Mustahid Hasan <mustahidhasan9@gmail.com>
2024-09-29 23:13:59 +0600

    get started with do not ask modal added

--- nms/DASHBOARD/urls.py
+++ nms/DASHBOARD/urls.py
@@ -1,10 +1,12 @@
 # dashboard/urls.py
 
 from django.urls import path
-from .views import dashboard_view, logout_view
+from .views import dashboard_view, logout_view, welcome_superuser, disable_modal
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),  # Assuming this is the root URL for the dashboard
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('welcome/', welcome_superuser, name='welcome_superuser'),
+    path('disable-modal/', disable_modal, name='disable_modal'),
     
 ]
--- nms/DASHBOARD/views.py
+++ nms/DASHBOARD/views.py
@@ -5,11 +5,35 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 
+
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard.html')  # Render the dashboard template
+    # Check if the user is a superuser and if they haven't disabled the modal for this session
+    if request.user.is_superuser and not request.session.get("dont_show_modal", False):
+        return render(request, "dashboard.html", {"is_superuser": True})
+    return render(request, "dashboard.html", {"is_superuser": False})
+
 
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect('login')  # Replace 'login' with the name of your login URL
+    return redirect("login")  # Replace 'login' with the name of your login URL
+
+
+@login_required
+def welcome_superuser(request):
+    if request.user.is_superuser:
+        return render(
+            request, "welcome_superuser.html"
+        )  # Replace with your welcome template
+    else:
+        return redirect(
+            "dashboard"
+        )  # Redirect non-superusers to the dashboard or another page
+
+
+@login_required
+def disable_modal(request):
+    # This view will set the session to not show the modal again
+    request.session["dont_show_modal"] = True
+    return redirect("dashboard")
