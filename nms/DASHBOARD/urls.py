d3b805d77183980e8e06c0174b3fde8659c6199c
Mustahid Hasan <mustahidhasan9@gmail.com>
2024-12-08 03:49:30 +0600

    snmp seperate result is removed

--- nms/DASHBOARD/urls.py
+++ nms/DASHBOARD/urls.py
@@ -6,5 +6,4 @@ from .views import logout_view, ping_operation, snmp_results
 urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("", ping_operation, name="ping_operation"),
-    path("snmp_results/", snmp_results, name="snmp_results"),
 ]
--- nms/DASHBOARD/views.py
+++ nms/DASHBOARD/views.py
@@ -114,7 +114,7 @@ def ping_operation(request):
                 if os_name == "Windows":
                     command = ["tracert", ip_address]
                 else:
-                    command = ["traceroute","-I", ip_address]
+                    command = ["traceroute", "-I", ip_address]
 
                 logger.info(f"Running traceroute for {ip_address}.")
                 response = subprocess.run(command, capture_output=True, text=True)
@@ -285,11 +285,6 @@ def ping_operation(request):
                     ):
                         # Redirect to snmp_results.html with results
                         table.add_row(["SNMP Walk Result", "\n".join(snmp_result)])
-                        return render(
-                            request,
-                            "snmp_results.html",
-                            {"table": table},
-                        )
                     else:
                         # Append the error message to the table
                         table.add_row(["SNMP Walk Result", "\n".join(snmp_result)])
