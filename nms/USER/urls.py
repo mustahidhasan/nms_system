8814ac4a2af1b653cb5d3155b474cef660ea481d
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-04-14 06:41:05 +0600

    login url changed

--- nms/USER/urls.py
+++ nms/USER/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/login/', views.azure_login, name='azure_login'),
+    path('', views.azure_login, name='azure_login'),
     path('auth/callback/', views.azure_callback, name='azure_callback'),
 ]
