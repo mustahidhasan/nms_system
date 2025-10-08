ae9043e5971d4f2f3f9d1cfdcfc1a28e3d74dc35
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2025-10-08 12:16:52 +0600

    DOcker file running them properly

--- nms/nms/settings.py
+++ nms/nms/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Optionally, define the JWT token lifetime
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x2--wh#=1^e7n^k*kttuey@wt%a$a7@)2()c=$_7ybu31v&yg0"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config("DJANGO_SECRET_KEY", default="fallback_secret")
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 
 # Application definition
