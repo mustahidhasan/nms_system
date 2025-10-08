7c0ec4423b7386f48772173319059c6813abff1e
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2025-10-08 13:41:39 +0600

    allowed host updated and worked from the global env

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# uploadthe project to server
+```
+ssh -i your-key.pem ec2-user@EC2_PUBLIC_IP
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+```
 # steps to run this project 
 - Update the .env file in nms folder with your own values
 - Run the following commands in the terminal
--- nms/nms/settings.py
+++ nms/nms/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------------
 SECRET_KEY = config("DJANGO_SECRET_KEY", default="fallback_secret")
 DEBUG = config("DEBUG", default=True, cast=bool)
-ALLOWED_HOSTS = ["*"]  # adjust for production
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 
 # -----------------------------
 # Installed apps
