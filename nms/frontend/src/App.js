86b84d7ef2cc1923becfcee2dd8cc4d14e7bde37
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2025-10-12 12:31:41 +0600

    settings.py and App.js updated

--- nms/frontend/src/App.js
+++ nms/frontend/src/App.js
@@ -1,18 +1,19 @@
-// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './component/Login';
-import Ping from './component/Ping'; // This is the dashboard component
+import Ping from './component/Ping'; // Dashboard component
 import UserActivity from './component/UserActivity'
 
 function App() {
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+  const API_BASE_URL =
+    process.env.REACT_APP_API_BASE_URL ||
+    (window.location.hostname === "localhost" ? "http://localhost:8000" : `${window.location.protocol}//${window.location.hostname}/api`);
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login apiBaseUrl={API_BASE_URL} />} />
         <Route path="/dashboard" element={<Ping apiBaseUrl={API_BASE_URL} />} />
-         <Route path="/user-activity" element={<UserActivity apiBaseUrl={API_BASE_URL} />} />
+        <Route path="/user-activity" element={<UserActivity apiBaseUrl={API_BASE_URL} />} />
       </Routes>
     </Router>
   );
