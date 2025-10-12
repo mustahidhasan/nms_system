4f205caaa78af153e4b1837fb3fe718da9f86cf9
Mustahid Hasan <46712349+mustahidhasan@users.noreply.github.com>
2025-10-12 15:48:57 +0600

    build.sh

--- nms/build.sh
+++ nms/build.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-set -e
 
 # Usage: ./build.sh dev OR ./build.sh prod
+
 ENV=${1:-dev}
 
 if [ "$ENV" = "prod" ]; then
   echo "Building Production Environment..."
-  ENV_FILE=".env.prod"
-  HOST_IP="50.17.3.155"
+  BACKEND_ENV_FILE=".env.prod.be"
+  FRONTEND_ENV_FILE=".env.prod.fe"
   COMPOSE_FILE="docker-compose.prod.yml"
 else
   echo "Building Development Environment..."
-  ENV_FILE=".env.dev"
-  HOST_IP="localhost"
+  BACKEND_ENV_FILE=".env.dev.be"
+  FRONTEND_ENV_FILE=".env.dev.fe"
   COMPOSE_FILE="docker-compose.dev.yml"
 fi
 
-export ENV_FILE
-export HOST_IP
+export BACKEND_ENV_FILE
+export FRONTEND_ENV_FILE
 
-echo "Building containers with ENV_FILE=$ENV_FILE..."
-docker-compose -f $COMPOSE_FILE build
+echo "Building containers with BACKEND_ENV_FILE=$BACKEND_ENV_FILE and FRONTEND_ENV_FILE=$FRONTEND_ENV_FILE..."
+
+# Build frontend with build-arg for env
+docker-compose -f $COMPOSE_FILE build --build-arg ENV_FILE=$FRONTEND_ENV_FILE
+
+# Start all containers
 docker-compose -f $COMPOSE_FILE up -d
 
 echo "$ENV environment is up!"
-docker-compose -f $COMPOSE_FILE logs -f
